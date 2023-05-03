adaf6ccfd2a5aee40d7a612c9fe78c01fc8890b5
dioq <zhendong2011@live.cn>
2023-05-03 18:26:44 +0800

    obfuscator

---
+++ obfuscator/main.c
@@ -0,0 +1,36 @@
+// #include <stdio.h>
+
+// void hello() {
+// 	printf("hello world\n");
+// }
+
+
+// int add(int a, int b)
+// {
+// 	return a + b;
+// }
+
+// int main() {
+// 	hello();
+// 	printf(" hello = %p\n", hello);
+// 	printf("&hello = %p\n", &hello);
+	
+// 	printf("****************%d*********************",__LINE__);
+
+// 	int(*func1)(int a, int b) = add;
+// 	int ret = func1(1,2);
+// 	printf("func1 result		= %d\n", ret);
+// 	printf(" add addr 		= %p\n", add);
+// 	printf("&add addr 		= %p\n", &add);
+// 	printf(" func1 addr 		= %p\n", func1);
+// 	printf("&func1 addr 		= %p\n", &func1);
+	
+// 	return 0;
+// }
+
+int main() {
+	int a = 0;
+	int b = 1;
+	int c = a + c;
+	return 0;
+}
